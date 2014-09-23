6c3b7b9b49c3a95768c80105563542090727717f
Peter Elger <peter.elger@nearform.com>
2014-09-23 21:36:00 +0000

    added dev support scripts

---
+++ scripts/dev/clone.sh
@@ -0,0 +1,15 @@
+git clone git@github.com:nearform/aws-ami-container.git
+git clone git@github.com:nearform/aws-elb-container.git
+git clone git@github.com:nearform/docker-container.git
+git clone git@github.com:nearform/blank-container.git
+git clone git@github.com:nearform/nscale.git
+git clone git@github.com:nearform/nscale-aws-analyzer.git
+git clone git@github.com:nearform/nscale-local-analyzer.git
+git clone git@github.com:nearform/nscale-client.git
+git clone git@github.com:nearform/nscale-kernel.git
+git clone git@github.com:nearform/nscale-noauth.git
+git clone git@github.com:nearform/nscale-protocol.git
+git clone git@github.com:nearform/nscale-sdk.git
+git clone git@github.com:nearform/nscale-util.git
+git clone git@github.com:nearform/nscale-web.git
+git clone git@github.com:nearform/nscale-api.git
